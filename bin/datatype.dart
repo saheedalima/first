5a8404e74ff3988ffd7dac1b53acd258d481cfd2
saheedalima <saheedalima318@gmail.com>
2023-03-27 10:16:35 -0700

    commit

---
+++ bin/datatype.dart
@@ -0,0 +1,16 @@
+void main(){
+  String name="Saheed";
+  int age=21;
+  int phone=7034858318;
+  String email="saheedalima318@gmail.com";
+  double mark=100;
+  print("My name is $name");
+  print("My name is $age");
+  print("My name is $phone");
+  print("My name is $email");
+  print("My name is $mark");
+
+
+
+
+}
